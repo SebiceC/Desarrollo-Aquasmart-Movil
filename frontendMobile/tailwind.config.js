5e3e82d56c5bd307d972edd4e467b4914d57acd7
joesnick <joseesteban08@hotmail.com>
2025-02-17 11:58:08 -0500

    Creación y configuración del entorno + dependencias

---
+++ frontendMobile/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#2E86C1",
+          danger: "#E74C3C",
+        }
+      }
+    },
+    plugins: [],
+  };
